12b1660787d96f6f93821d773c9f3706ef93b824
ksathvikreddy <ksathvikreddy31@gmail.com>
2026-03-03 11:58:44 +0530

    Fix: Bypass SSL certificate verification for Azure MySQL

--- app.py
+++ app.py
@@ -45,4 +45,4 @@ def login():
 
 if __name__ == "__main__":
     # 0.0.0.0 is the "Industry Standard" for cloud containers
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=80)
