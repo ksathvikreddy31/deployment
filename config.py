240180b88e24ef552974d152022644afef82f3ea
ksathvikreddy <ksathvikreddy31@gmail.com>
2026-03-03 10:45:54 +0530

    Fix SSL for Azure MySQL

--- config.py
+++ config.py
@@ -9,9 +9,14 @@ class Config:
     MYSQL_DB = os.getenv("MYSQL_DB")
 
     SQLALCHEMY_DATABASE_URI = (
-    f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}"
-    f"@{MYSQL_HOST}/{MYSQL_DB}"
-    "?ssl_mode=REQUIRED"
+        f"mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}"
+        f"@{MYSQL_HOST}/{MYSQL_DB}"
     )
+
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "connect_args": {
+            "ssl": {"ssl_disabled": False}
+        }
+    }
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
